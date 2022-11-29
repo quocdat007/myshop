662eca5a6b4c457e657e0c039622a7066cdf5276
quocdat007 <nightfury@duck.com>
2022-11-29 09:23:41 +0700

    Quan ly trang thai voi provider - ProductsManager

--- lib/ui/products/user_products_screen.dart
+++ lib/ui/products/user_products_screen.dart
@@ -1,8 +1,7 @@
-
-
 // ignore_for_file: avoid_print
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../shared/app_drawer.dart';
 
 import 'user_product_list_tile.dart';
@@ -14,7 +13,6 @@ class UserProductsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final productsManager = ProductsManager();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Products'),
@@ -25,22 +23,26 @@ class UserProductsScreen extends StatelessWidget {
       drawer: const AppDrawer(),
       body: RefreshIndicator(
         onRefresh: () async => print('refresh products'),
-        child: buildUserProductListView(productsManager),
+        child: buildUserProductListView(),
       ),
     );
   }
 
-  Widget buildUserProductListView(ProductsManager productsManager) {
-    return ListView.builder(
-      itemCount: productsManager.itemCount,
-      itemBuilder: (ctx, i) => Column(
-        children: [
-          UserProductListTile(
-            productsManager.items[i],
+  Widget buildUserProductListView() {
+    return Consumer<ProductsManager> (
+      builder: (ctx, productsManager, child) {
+        return ListView.builder(
+          itemCount: productsManager.itemCount,
+          itemBuilder: (ctx, i) => Column(
+            children: [
+              UserProductListTile(
+                productsManager.items[i],
+              ),
+              const Divider(),
+            ],
           ),
-          const Divider(),
-        ],
-      ),
+        );
+      } ,
     );
   }
 
